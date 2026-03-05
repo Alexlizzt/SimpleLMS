bbd02836b507730434bb726d637bcc3e6ec4bdd1
Ivan Lizzt <alejandrolistc@hotmail.com>
2026-03-05 18:19:53 -0500

    chore: Agrega modulos para url amigable y debug

--- website/vendor/drupal/DrupalInstalled.php
+++ website/vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '30f6129e3dc2f94f';
+  public const string VERSIONS_HASH = 'c4ace5ff2cfdab38';
 
 }
