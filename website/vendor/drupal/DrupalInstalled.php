ce8c8b4a74d16a4319a5474a292fc53c415450aa
Ivan Lizzt <alejandrolistc@hotmail.com>
2026-03-18 16:44:50 -0500

    chore: Agrega roles correctamente al formulario

--- website/vendor/drupal/DrupalInstalled.php
+++ website/vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '823f1e340b286627';
+  public const string VERSIONS_HASH = 'e3d4642c6496c5d6';
 
 }
